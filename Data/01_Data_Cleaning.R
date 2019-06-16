30bbf17e48215dbde2542612aa2a35c6fc9952c7
CrisBMoya <tobal.92@gmail.com>
2019-06-15 22:57:31 -0400

    update

--- Data/01_Data_Cleaning.R
+++ Data/01_Data_Cleaning.R
@@ -8,27 +8,38 @@ library(plyr)
 
 
 #List files
-TextFiles=list.files(path="F:/NOAA/", full.names=TRUE)
+TextFiles=list.files(path="F:/NOAA/", full.names=TRUE, pattern="\\.txt")
+
+#Numeric Values
+NumericCol=c("Elevation", "Mean.Temperature.Fahrenheit", "Mean.Dew.Point.Fahrenheit",
+  "Mean.Sea.Level.Pressure.Millibars","Mean.Station.Pressure.Millibars",
+  "Mean.Visibility.Miles", "Mean.Wind.Speed.Knots", "Maximum.Sustained.Wind.Speed.Knots",
+  "Maximum.Wind.Gust.Knots","Maximum.Temperature.Fahrenheit","Minimum.Temperature.Fahrenheit",
+  "Precipitation.Amount.Inches", "Snow.Depth.Inches")
 
 #Load data
-AllFiles=lapply(TextFiles, function(x){
-  read_delim(file=x, delim="\t")
-  })
+AllFiles=lapply(TextFiles, function(i){
+  temp=read_delim(file=i, delim="\t")
+  temp=na.omit(temp)
+  
+  #Fix stupid colnames
+  colnames(temp)=c("Station","Date","Latitude","Longitude",
+                   "Elevation", "Location.Name", "Mean.Temperature.Fahrenheit", "Mean.Dew.Point.Fahrenheit",
+                   "Mean.Sea.Level.Pressure.Millibars","Mean.Station.Pressure.Millibars",
+                   "Mean.Visibility.Miles", "Mean.Wind.Speed.Knots", "Maximum.Sustained.Wind.Speed.Knots",
+                   "Maximum.Wind.Gust.Knots","Maximum.Temperature.Fahrenheit","Minimum.Temperature.Fahrenheit",
+                   "Precipitation.Amount.Inches", "Snow.Depth.Inches", "NN2")
+  
+  #Change some class
+  temp[,NumericCol] = apply(temp[,NumericCol], 2, function(x) as.numeric(x))
+  
+  
+  temp
+  
+})
 
 #Turn into one table
 AllData=as.tibble(ldply(.data=AllFiles, .fun=data.frame))
-
-#Fix stupid colnames
-colnames(AllData)=c("Station","Date","Latitude","Longitude",
-                     "Elevation", "NN1", "Mean.Temperature.Fahrenheit", "Mean.Dew.Point.Fahrenheit",
-                     "Mean.Sea.Level.Pressure.Millibars","Mean.Station.Pressure.Millibars",
-                     "Mean.Visibility.Miles", "Mean.Wind.Speed.Knots", "Maximum.Sustained.Wind.Speed.Knots",
-                     "Maximum.Wind.Gust.Knots","Maximum.Temperature.Fahrenheit","Minimum.Temperature.Fahrenheit",
-                     "Precipitation.Amount.Inches", "Snow.Depth.Inches", "NN2")
-AllData=AllData[colnames(AllData)[!grepl(pattern="NN", x=colnames(AllData))]]
-
-#Delete NA
-AllData=na.omit(AllData)
 
 #Save RDS
 saveRDS(object=AllData, file="F:/NOAA/All.NOAA.Data.RDS", compress=TRUE)
--- Data/02_Data_Aggregate.R
+++ Data/02_Data_Aggregate.R
@@ -6,12 +6,18 @@ library(tidyverse)
 #Read RDS
 AllData=readRDS(file="F:/NOAA/All.NOAA.Data.RDS")
 
+#Numeric Values
+NumericCol=c("Elevation", "Mean.Temperature.Fahrenheit", "Mean.Dew.Point.Fahrenheit",
+             "Mean.Sea.Level.Pressure.Millibars","Mean.Station.Pressure.Millibars",
+             "Mean.Visibility.Miles", "Mean.Wind.Speed.Knots", "Maximum.Sustained.Wind.Speed.Knots",
+             "Maximum.Wind.Gust.Knots","Maximum.Temperature.Fahrenheit","Minimum.Temperature.Fahrenheit",
+             "Precipitation.Amount.Inches", "Snow.Depth.Inches")
+
 #Extract day and month and average all data across three years per day.
 AllData$DayMonth=format(as.Date(AllData$Date, format="%d/%m/%Y"),"%m-%d")
 
-TEMP=head(AllData)
-TEMP$Latitude
-TEMP$Longitude
-TEMP2=AllData[AllData$DayMonth==format(as.Date("06-23", format="%m-%d"),"%m-%d"),]
-unique(TEMP2$Date)
-aggregate(x=TEMP, by=list(TEMP$DayMonth), FUN=mean)
+#Test if the result is three values per location, as the aggregate function does take date with year into account
+TestData=aggregate(x=AllData[,NumericCol], by=list(AllData$Location.Name, AllData$Date), FUN=mean)
+head(TestData)
+#Aggregate data per location and date (equal dates across all three years)
+MeanData=aggregate(x=AllData[,NumericCol], by=list(AllData$Location.Name, AllData$DayMonth), FUN=mean)
